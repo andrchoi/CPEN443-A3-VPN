e9e2dbd6fd176e20b8d51b1a0e590ba6a257a323
Zachary <zchua025@e.ntu.edu.sg>
2019-10-11 05:17:50 +0800

    adding dh implementation into server

--- backend/vpn_client.py
+++ backend/vpn_client.py
@@ -4,13 +4,7 @@ import socket
 # 1. symmetric key
 # 2. ip of other computer
 
-'''
-A = g**secret_value % p
-public_key1 is g
-private_key is secret_value
-public_key2 is p
-partial_key is A
-'''
+
 class DH_Endpoint(object):
     def __init__(self, public_key1, public_key2, private_key):
         self.public_key1 = public_key1
@@ -61,8 +55,17 @@ PORT = get_portnum()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-
     s.sendall(b'Hello, world')
     data = s.recv(1024)
 
 print('Received', repr(data)) #repr returns string of data
+# if __name__ == "__main__":
+#     zach = DH_Endpoint(1294,79,54)
+#     alex = DH_Endpoint(1294,79,31)
+#     zach_pk = zach.generate_partial_key()
+#     alex_pk = alex.generate_partial_key()
+#     full_key = zach.generate_full_key(alex_pk)
+#     full_key = alex.generate_full_key(zach_pk)
+#     zach_em = zach.encrypt_message("hello alex i am doing good help me do my 432 writeen adssignment")
+#     alex_dm = alex.decrypt_message(zach_em)
+#     print(alex_dm)
